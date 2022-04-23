e6f921262eead4c5ac6bfec3cf0c0e6a89d5ac93
5undel <max.sundel@Choice.se>
2022-04-23 09:02:59 +0000

    Add view to home page

--- time_to_train/urls.py
+++ time_to_train/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('base.urls')),
 ]
