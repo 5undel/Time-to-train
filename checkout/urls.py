23f18206ed43cfcc6e1a6cb2d6dd7f1e8572859c
5undel <max.sundel@Choice.se>
2022-04-24 07:39:26 +0000

    Add view for checkout_success

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,4 +5,5 @@ from . import views
 # url path to the home page
 urlpatterns = [
     path('<int:pk>/', views.checkout, name='checkout'),
+    path('checkout_success/<membership_number>', views.checkout_success, name='checkout_success'),
 ]
